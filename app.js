b22eeb3c711d1d8b28137f3a56e4538f4cc694a8
Kurvin Hendricks <kurvinhendricks@gmail.com>
2016-02-21 16:32:45 +0200

    first commit ...

---
+++ app.js
@@ -0,0 +1,6 @@
+angular.module('app', []);
+
+angular.module('app').controller("mainController", function(){
+    var vm = this;
+    vm.title = 'AngularJS Tutorial Example';
+});
---
+++ js/main.ctrl.js
@@ -0,0 +1,6 @@
+angular.module('app').controller("MainController", function(){
+    var vm = this;
+    vm.title = 'AngularJS Tutorial Example';
+
+    vm.data = "not the right plastic";
+});
